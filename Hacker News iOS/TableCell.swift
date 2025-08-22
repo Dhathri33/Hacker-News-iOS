c3ebf0702120210731d86985143183406f095ac1
Dhathri33 <dbathini@luc.edu>
2025-08-22 12:02:56 -0500

    Created HackerNews IOS with TableView using Storyboard

---
+++ Hacker News iOS/TableCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableCell.swift
+//  Hacker News iOS
+//
+//  Created by Dhathri Bathini on 8/22/25.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+    
+    @IBOutlet weak var titleOfThePost: UILabel!
+    @IBOutlet weak var postDetails: UILabel!
+    @IBOutlet weak var points: UILabel!
+    @IBOutlet weak var numberOfComments: UILabel!
+    
+}
